db12c7c10ebba3a494033e8cefedbc216e512fbb
USUARIO <USUARIO@192.168.1.107>
2021-07-09 19:59:45 -0500

    clase

--- src/diagramas/de/flujo/Main3.java
+++ src/diagramas/de/flujo/Main3.java
@@ -7,15 +7,12 @@ public class Main3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		 Scanner lector=new Scanner(System.in);
-		 int N = lector.nextInt();
 		 int n;
-	        System.out.print("Introduce un número : ");                                                         
+	        System.out.print("Introduce el número 7 : ");                                                         
 	        n = lector.nextInt();
 	        System.out.println("Tabla del " + n);
 	        for(int i = 1; i<=12; i++){
 	             System.out.println(n + " * " + i + " = " + n*i);                                                     
-	        }
+	        } 
 	}
 }
-
-
