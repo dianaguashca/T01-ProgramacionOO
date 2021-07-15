b8f9bc78dc37cab3be7a055f28afc27a30904b5c
USUARIO <USUARIO@192.168.1.107>
2021-07-14 19:06:08 -0500

    Trabajo en clase

---
+++ src/diagramas/de/flujo/Mainfigura.java
@@ -0,0 +1,25 @@
+package diagramas.de.flujo;
+
+import java.util.Scanner;
+
+public class Mainfigura {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 System.out.print("Ingrese el numero de filas: ");
+		 Scanner lector=new Scanner(System.in);
+		 int N = lector.nextInt();
+		 //es el bucle grande 
+		 for(int fila=1;fila<=N;fila++) {
+			 
+			 //bucle chiquito dentro del bucle grande
+			for(int ast=1;ast<=fila;ast++) {
+				 System.out.print("*");
+			}
+			 
+			 System.out.println();//salto de linea 
+			 
+		 } 
+	}
+
+}
