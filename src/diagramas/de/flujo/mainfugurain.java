bb2d00eca2f7433153f268266cd875e98438f5d7
USUARIO <USUARIO@192.168.1.107>
2021-07-14 19:04:56 -0500

    trabajoen clase

---
+++ src/diagramas/de/flujo/mainfugurain.java
@@ -0,0 +1,27 @@
+package diagramas.de.flujo;
+
+import java.util.Scanner;
+
+public class mainfugurain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 System.out.print("Ingrese el numero de filas: ");
+		 Scanner lector=new Scanner(System.in);
+		 int N = lector.nextInt();
+		 //es el bucle grande 
+		 for(int fila=1;fila<=N;fila++) { 
+			 
+			 //bucle chiquito dentro del bucle grande
+			for(int ast=N;ast>=fila;ast--) {
+				 System.out.print("*");
+			}
+			 
+			 System.out.println();//salto de linea 
+			 
+		 }
+	
+
+	}
+
+}
