d1660250b073e194927dd7e5a6dd4a3fff749900
USUARIO <USUARIO@192.168.1.107>
2021-07-22 18:12:33 -0500

    Taller de datos

---
+++ src/ejemplo/tipo/de/datos/MainTiposDeDatos.java
@@ -0,0 +1,21 @@
+package ejemplo.tipo.de.datos;
+
+public class MainTiposDeDatos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     short cantidadEstudiantes=1668;
+     byte entradaCircuito=01;
+     int cantCiudadanos=396789;
+     long cantidadMundo=1699999;
+     float sueldo=2810.55F;
+     double sanldoBanco=16666666.67;
+     char OpcionMenu='C';
+     boolean casado=true;
+     
+     
+     
+     
+	}
+
+}
