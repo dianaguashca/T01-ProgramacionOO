0af4a2adbe754b29ea7ef126361a887e6adbab98
USUARIO <USUARIO@192.168.1.107>
2021-07-28 16:22:19 -0500

    correciones de las pruebas

--- src/correccion/de/la/prueba/CorreccionPrueba1.java
+++ src/correccion/de/la/prueba/CorreccionPrueba1.java
@@ -2,31 +2,33 @@ package correccion.de.la.prueba;
 
 import java.util.Scanner;
 
+
 public class CorreccionPrueba1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		 Scanner lector=new Scanner(System.in);
-		 int N;
-		 int S=0;
-		//correccion 
-		 int C=0;
-		float P=0f;
-		
-	     for(int i=1;i!=0;i++) {
-	    	 System.out.println("Ingrese un numero: ");
-	    	 int N1 = lector.nextInt();
-	    	   if(N1==0) {
-	    		   i=-1;
-	   			
-	   		}else {
-	   		   int SUM=S+N1;
-	   		   int PRO=S/i;  
-	   		   int CAN=C+1;
-					
-	  
-	     System.out.println(" la suma es  "+SUM);
-			System.out.println("el primedio es :"+PRO);	
-			 System.out.println(" la cantidad es:  "+CAN);
-	     
-	}}}}
+		int S = 0;
+		float P = 0;
+		int C = 0;
+		int i = 0;
+		for (i = 1 ; i != 0 ; i++) {
+			System.out.println("Ingrese un numero positivo: ");
+			System.out.println("****************************");
+			Scanner lector = new Scanner(System.in);
+			int N = lector.nextInt();
+			if (N == 0){
+				i = -1;
+				
+			}else if (N > 0){
+				S = S+N;
+				P = S/i;
+				C = C + 1;
+				
+			}
+		}
+		System.out.println("La suma es: " + S);
+		System.out.println("El promedio es: " + P);
+		System.out.println("La cantidad es: " + C);
+	}
+
+}
--- src/correccion/de/la/prueba/CorreccionPrueba2.java
+++ src/correccion/de/la/prueba/CorreccionPrueba2.java
@@ -1,10 +1,43 @@
 package correccion.de.la.prueba;
 
+import java.util.Scanner;
+
 public class CorreccionPrueba2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-       //correr 6
+		int SP = 0;
+		int SI = 0;
+		int CI = 0;
+		float PI = 0;
+		
+		for (int i = 1 ; i != -1 ; i++) {
+			
+			System.out.println("INGRESE UN NUMERO POSITIVO: ");
+			System.out.println("**************************** ");
+			Scanner lector = new Scanner(System.in);
+			int N = lector.nextInt();
+			
+			
+			if (N == -1){
+				 
+				i = -2;
+				
+			}else if (N%2==0){
+				
+				SP = SP + N;
+					
+			}else if (N%2!=0){
+				SI = SI+N;
+				CI = CI+1;
+				PI = SI/CI;
+				
+			}
+		}
+		
+		System.out.println("La suma pares es: " + SP);
+		System.out.println("El promedio  impares es: " + PI);
+		
 	}
 
 }
