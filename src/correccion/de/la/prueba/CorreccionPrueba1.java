f174341371dd4df40ff837ba07f1a0bacd6532d8
USUARIO <USUARIO@192.168.1.107>
2021-07-28 13:25:15 -0500

    correccion

--- src/correccion/de/la/prueba/CorreccionPrueba1.java
+++ src/correccion/de/la/prueba/CorreccionPrueba1.java
@@ -6,10 +6,27 @@ public class CorreccionPrueba1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		 System.out.print("INGRESE UN NUMERO POSITIVO: ");
 		 Scanner lector=new Scanner(System.in);
-		 int N = lector.nextInt(); 
-		 //hola 
-	}
-
-}
+		 int N;
+		 int S=0;
+		 int i
+		 int C=0;
+		float P=0f;
+		
+	     for(int i=1;i!=0;i++) {
+	    	 System.out.println("Ingrese un numero: ");
+	    	 int N1 = lector.nextInt();
+	    	   if(N1==0) {
+	    		   i=-1;
+	   			
+	   		}else {
+	   		   int SUM=S+N1;
+	   		   int PRO=S/r;
+	   		   int CAN=C+1;
+					
+	  
+	     System.out.println(" la suma es  "+SUM);
+			System.out.println("el primedio es :"+PRO);	
+			 System.out.println(" la cantidad es:  "+CAN);
+	     
+	}}}}
