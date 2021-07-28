6400bb1bfd19da5b694491bc46ad4283bc863662
USUARIO <USUARIO@192.168.1.107>
2021-07-28 12:15:49 -0500

    correccion

---
+++ src/correccion/de/la/prueba/CorreccionPrueba1.java
@@ -0,0 +1,14 @@
+package correccion.de.la.prueba;
+
+import java.util.Scanner;
+
+public class CorreccionPrueba1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 System.out.print("INGRESE UN NUMERO POSITIVO: ");
+		 Scanner lector=new Scanner(System.in);
+		 int N = lector.nextInt();
+	}
+
+}
