537e2b9991759a338c666a33ee9ee031c5659781
USUARIO <USUARIO@192.168.1.107>
2021-07-30 16:13:02 -0500

    while primera prueba

--- src/ejecicioswhile/prueba1while.java
+++ src/ejecicioswhile/prueba1while.java
@@ -7,23 +7,27 @@ public class prueba1while {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner lector = new Scanner(System.in);
-			int S = 0;
-			float P = 0;
-			int C = 0;
-			int i = 0;
-				System.out.println("Ingrese un numero positivo: ");
-				int N = lector.nextInt();
+		    int N=0;
+			int Suma = 0;
+			float E = 0;
+			int P= 0;
+			
+				System.out.println("Ingrese un numero positivo diferente de 0: ");
+				 N = lector.nextInt();
+				 Suma=Suma+N;
 				 while(N!= 0 ) {
 					 System.out.println("Ingrese un numero positivo: ");
 					 N = lector.nextInt();
-		      S = S+N;
-			P = S/i;
-			C = C + 1;
+
+				 
+				 Suma = Suma+N;
+					E++;
+					P = (int) (Suma/E);
 	           
-	}
-			System.out.println("La suma es: " + S);
+				 }
+			System.out.println("La suma es: " + Suma);
 			System.out.println("El promedio es: " + P);
-			System.out.println("La cantidad es: " + C);
+			System.out.println("La cantidad es: " + E);
 			
 	}
 	
