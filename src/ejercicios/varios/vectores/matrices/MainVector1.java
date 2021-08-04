162b97b1d3910cef5675bc0c1ba39ccb86665e17
USUARIO <USUARIO@192.168.1.107>
2021-08-03 19:09:38 -0500

    Taller de Matrices

---
+++ src/ejercicios/varios/vectores/matrices/MainVector1.java
@@ -0,0 +1,36 @@
+package ejercicios.varios.vectores.matrices;
+
+import java.util.Scanner;
+
+public class MainVector1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     //declare un vector X[3] de tipo char y construya un programa que pida al usuario que
+		//ingrese los valores para cada posicion
+	//y finalmente que se imprima todos los valores ingresados 
+		
+		char X[]=new char [3];
+	
+	for(int i=0;i<3;i++) {
+		
+	System.out.println("Ingrese valor de la posicion:"+i);
+	 Scanner lector=new Scanner(System.in);
+	 char N = lector.next().charAt(0);
+	 
+	 X[i]=N;
+	}
+	
+	//rrecoriendo el vector lleno 
+	for(int i=0;i<3;i++) {
+		System.out.println("El  valor de la posicion:"+i);
+		System.out.println(X[i]);
+}
+	
+
+	
+	
+	
+	}
+
+}
