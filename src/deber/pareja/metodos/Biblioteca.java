c443e1acb43ee3cb7a5ecb25aaad8ef0a79f3de3
USUARIO <USUARIO@192.168.1.107>
2021-08-13 12:15:43 -0500

    Deber el pareja

---
+++ src/deber/pareja/metodos/Biblioteca.java
@@ -0,0 +1,24 @@
+package deber.pareja.metodos;
+//esta es la clase no main de Diana Guashca
+public class Biblioteca {
+ String libros;
+ String autores;
+ String lectores;
+ int Ncedula;
+ int codigoLibro;
+ public void prestamo1(String librosAl, int Ncedula) {
+		System.out.println(" BIENVEDIDO  A LA BLIBLIOTECA");
+		System.out.println(librosAl );
+		System.out.println(".....................................");
+		System.out.println(" Primer Prestamo de la Biblioteca");
+		System.out.println(".....................................");
+		
+		} 
+ public void prestamo2(String librosAl, int Ncedula) {
+	 System.out.println();
+	 System.out.println(".....................................");
+		System.out.println(" Segundo Prestamo de la Biblioteca ");
+		System.out.println(librosAl );
+		System.out.println("...................................");
+ }
+}
