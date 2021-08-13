90ccc800f50f560796b498ebb731a4bb19cc5cc5
USUARIO <USUARIO@192.168.1.107>
2021-08-13 12:25:12 -0500

    vamos

--- src/deber/pareja/metodos/Autos.java
+++ src/deber/pareja/metodos/Autos.java
@@ -3,7 +3,7 @@ package deber.pareja.metodos;
 public class Autos {
 	String color;
 	String marca;   
-	String modelo;
+	String modelo; 
 	double precio;
 	public void comprar(String autosDisponibles, int Ncedula) {
 		System.out.println(" BIENVEDIDO ");
