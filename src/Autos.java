d72f3b695d718ed0d1652e8d35b36ec005b10c5f
dianaguashca <86386099+dianaguashca@users.noreply.github.com>
2021-08-13 14:15:53 -0500

    Deber en Parejas

---
+++ src/Autos.java
@@ -0,0 +1,23 @@
+package deber.pareja.metodos;
+
+//esta es mi clase no Main Nathan Velez
+public class Autos {
+	String color; 
+	String marca;   
+	String modelo; 
+	double precio; 
+	//main 
+	public void comprar(String autosDisponibles, int Ncedula) {
+		System.out.println(" BIENVEDIDO ");
+		System.out.println("OFRECEMOS: ");
+		System.out.println( autosDisponibles);
+		System.out.println("...................");
+		
+		}
+	public void comprar2(String autosDisponibles, int Ncedula) {
+		System.out.println("....................");
+		System.out.println( autosDisponibles);
+		System.out.println("....................");
+	}  
+}
+   
